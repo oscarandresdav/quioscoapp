18666524b0f70115bbf3b37a2a734a9a32853f42
Oscar Davila <oscar7822002@gmail.com>
2022-09-29 15:42:07 -0500

    Detectando el Producto que dimos Click

--- components/Producto.jsx
+++ components/Producto.jsx
@@ -1,27 +1,38 @@
-import Image from 'next/image'
-import React from 'react'
-import { formatearDinero } from '../helpers';
+import Image from "next/image";
+import React from "react";
+import { formatearDinero } from "../helpers";
+import useQuiosco from "../hooks/useQuiosco";
 
-const Producto = ({producto}) => {
+const Producto = ({ producto }) => {
+  const { handleSetProducto, handleChangeModal } = useQuiosco();
   const { nombre, imagen, precio } = producto;
 
   return (
-    <div className='border p-3 text-center'>
-      <Image 
+    <div className="border p-3 text-center">
+      <Image
         width={400}
         height={500}
         src={`/assets/img/${imagen}.jpg`}
         alt={`Imagen ${nombre}`}
       />
       <div className="p-5">
-        <h3 className='text-2xl font-bold'>{nombre}</h3>
-        <p className='mt-5 font-black text-4xl text-amber-500'>
+        <h3 className="text-2xl font-bold">{nombre}</h3>
+        <p className="mt-5 font-black text-4xl text-amber-500">
           {formatearDinero(precio)}
         </p>
+        <button
+          type="button"
+          className="bg-indigo-600 hover:bg-indigo-800 text-white w-full mt-5 p-3 uppercase font-bold"
+          onClick={() => {
+            handleChangeModal()
+            handleSetProducto(producto)
+          }}
+        >
+          Agregar
+        </button>
       </div>
-
     </div>
-  )
-}
+  );
+};
 
-export default Producto
+export default Producto;
--- context/QuioscoProvider.jsx
+++ context/QuioscoProvider.jsx
@@ -6,6 +6,8 @@ const QuioscoContext = createContext();
 const QuiscoProvider = ({ children }) => {
   const [categorias, setCategorias] = useState([]);
   const [categoriaActual, setCategoriaActual] = useState([]);
+  const [producto, setProducto] = useState({});
+  const [modal, setModal] = useState(false);
 
   const obtenerCategorias = async () => {
     const { data } = await axios("/api/categorias");
@@ -25,12 +27,22 @@ const QuiscoProvider = ({ children }) => {
     setCategoriaActual(categoria[0]);
   };
 
+  const handleSetProducto = (producto) => {
+    setProducto(producto);
+  };
+
+  const handleChangeModal = () => {
+    setModal(!modal);
+  };
+
   return (
     <QuioscoContext.Provider
       value={{
         categorias,
         categoriaActual,
         handleClickCategoria,
+        handleSetProducto,
+        handleChangeModal,
       }}
     >
       {children}
